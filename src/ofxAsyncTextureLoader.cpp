15bda8472547548e92c6a9118fdbd6e8382f950b
Gal Sasson <gal@localprojects.com>
2016-04-20 12:31:40 -0400

    initial commit

---
+++ src/ofxAsyncTextureLoader.cpp
@@ -0,0 +1,114 @@
+//
+//  ofxAsyncTextureLoader.cpp
+//  ofxAsyncTextureLoader
+//
+//  Created by Gal Sasson on 4/20/16.
+//
+//
+
+#include "ofxAsyncTextureLoader.h"
+
+ofxAsyncTextureLoader::~ofxAsyncTextureLoader()
+{
+	bRunning = false;
+	textureLoaderThread.detach();
+}
+
+ofxAsyncTextureLoader::ofxAsyncTextureLoader()
+{
+	GLFWwindow* mainWindow = getMainContextWindow();
+	bool ok = create2ndContext(mainWindow);
+	if (!ok) {
+		ofLogError("ofxAsyncTextureLoader") << "error creating 2nd openGL context";
+		return;
+	}
+	ofLogNotice("ofxAsyncTextureLoader") << "2nd openGL context created successfully";
+	ofAddListener(ofEvents().update, this, &ofxAsyncTextureLoader::callCompleteCallbacks);
+	textureLoaderThread = std::thread(&ofxAsyncTextureLoader::loaderThreadFunction, this);
+	bRunning = true;
+}
+
+void ofxAsyncTextureLoader::loadTextureAsync(const string &path, const function<void(shared_ptr<ofTexture>)>& completeCallback, bool mipmapped)
+{
+	TextureLoaderTask task;
+	task.path = path;
+	task.bMipmapped = mipmapped;
+	task.tex = NULL;
+	task.loadCompleteCallback = completeCallback;
+	loadQueueMutex.lock();
+	loadQueue.push_back(task);
+	loadQueueMutex.unlock();
+}
+
+shared_ptr<ofTexture> ofxAsyncTextureLoader::loadTextureSync(const string& path, bool mipmapped)
+{
+	shared_ptr<ofTexture> tex(make_shared<ofTexture>());
+	bool ok = ofLoadImage(*tex, path);
+	if (ok) {
+		if (mipmapped) {
+			tex->bind();
+			glGenerateMipmap(tex->getTextureData().textureTarget);
+			tex->unbind();
+			tex->setTextureMinMagFilter(GL_LINEAR_MIPMAP_LINEAR, GL_LINEAR);
+		}
+
+		tex->setTextureWrap(GL_CLAMP_TO_EDGE, GL_CLAMP_TO_EDGE);
+	}
+	else {
+		return NULL;
+	}
+
+	return tex;
+}
+
+GLFWwindow* ofxAsyncTextureLoader::getMainContextWindow()
+{
+	ofAppBaseWindow* win = ofGetWindowPtr();
+	ofAppGLFWWindow* glfwWin = dynamic_cast<ofAppGLFWWindow*>(win);
+	return glfwWin->getGLFWWindow();
+}
+
+bool ofxAsyncTextureLoader::create2ndContext(GLFWwindow* main)
+{
+	glfwWindowHint(GLFW_VISIBLE, GL_FALSE);
+	context = glfwCreateWindow(10, 10, "", NULL, main);
+	if (context == NULL) {
+		return false;
+	}
+	return true;
+}
+
+void ofxAsyncTextureLoader::loaderThreadFunction()
+{
+	glfwMakeContextCurrent(context);
+	while (bRunning) {
+		loadQueueMutex.lock();
+		std::vector<TextureLoaderTask> tasks = loadQueue;
+		loadQueue.clear();
+		loadQueueMutex.unlock();
+
+		for (TextureLoaderTask& task : tasks) {
+			task.tex = loadTextureSync(task.path, task.bMipmapped);
+			if (task.tex == NULL) {
+				ofLogError("BackgroundTextureLoader") << "error loading texture: " << task.path;
+			}
+			completeQueueMutex.lock();
+			completeQueue.push_back(task);
+			completeQueueMutex.unlock();
+		}
+
+		ofSleepMillis(1);
+	}
+}
+
+void ofxAsyncTextureLoader::callCompleteCallbacks(ofEventArgs& updateArgs)
+{
+	completeQueueMutex.lock();
+	vector<struct TextureLoaderTask> loaded = completeQueue;
+	completeQueue.clear();
+	completeQueueMutex.unlock();
+
+	for (TextureLoaderTask& task : loaded) {
+		task.loadCompleteCallback(task.tex);
+	}
+}
---
+++ src/ofxAsyncTextureLoader.h
@@ -0,0 +1,43 @@
+//
+//  ofxAsyncTextureLoader.h
+//  ofxAsyncTextureLoader
+//
+//  Created by Gal Sasson on 4/20/16.
+//
+//
+
+#pragma once
+
+#include "ofMain.h"
+
+class ofxAsyncTextureLoader
+{
+public:
+	~ofxAsyncTextureLoader();
+	ofxAsyncTextureLoader();
+	// load texture async (returns a unique request id)
+	void loadTextureAsync(const string& path, const function<void(shared_ptr<ofTexture>)>& completeCallback = [](shared_ptr<ofTexture> tex) {}, bool mipmapped=false);
+	// load texture sync
+	shared_ptr<ofTexture> loadTextureSync(const string& path, bool mipmapped=false);
+
+	void callCompleteCallbacks(ofEventArgs& updateArgs);
+
+private:
+	bool bRunning;
+	GLFWwindow* context;
+	std::thread textureLoaderThread;
+	struct TextureLoaderTask {
+		string path;
+		bool bMipmapped;
+		shared_ptr<ofTexture> tex;
+		function<void(shared_ptr<ofTexture>)> loadCompleteCallback;
+	};
+	std::vector<struct TextureLoaderTask> loadQueue;
+	std::mutex loadQueueMutex;
+	std::vector<struct TextureLoaderTask> completeQueue;
+	std::mutex completeQueueMutex;
+
+	GLFWwindow* getMainContextWindow();
+	bool create2ndContext(GLFWwindow* main);
+	void loaderThreadFunction();
+};
